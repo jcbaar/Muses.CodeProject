7ac1a2e12143f63651bd6092ea890f56628a06d5
Jan van den Baard <jcbaar@outlook.com>
2017-11-03 17:42:33 +0100

    Changed the project to a multi-target project.

---
+++ DeployNuGetPackages.bat
@@ -0,0 +1,1 @@
+nuget init Muses.CodeProject\bin\Release\ D:\NuGet\LocalPackages
--- Muses.CodeProject/Properties/AssemblyInfo.cs
+++ Muses.CodeProject/Properties/AssemblyInfo.cs
@@ -4,15 +4,15 @@ using System.Resources;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Muses.CodeProject")]
-[assembly: AssemblyDescription("Library for accessing the Codeproject API")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Jan van den Baard")]
-[assembly: AssemblyProduct("Muses.CodeProject")]
-[assembly: AssemblyCopyright("Copyright © 2017 Jan van den Baard")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-[assembly: NeutralResourcesLanguage("en")]
+//[assembly: AssemblyTitle("Muses.CodeProject")]
+//[assembly: AssemblyDescription("Library for accessing the Codeproject API")]
+//[assembly: AssemblyConfiguration("")]
+//[assembly: AssemblyCompany("Jan van den Baard")]
+//[assembly: AssemblyProduct("Muses.CodeProject")]
+//[assembly: AssemblyCopyright("Copyright © 2017 Jan van den Baard")]
+//[assembly: AssemblyTrademark("")]
+//[assembly: AssemblyCulture("")]
+//[assembly: NeutralResourcesLanguage("en")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -24,5 +24,5 @@ using System.Resources;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+//[assembly: AssemblyVersion("1.0.0.0")]
+//[assembly: AssemblyFileVersion("1.0.0.0")]
